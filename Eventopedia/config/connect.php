e642ce74961b1c7c42513264c4868cbf49b3e929
Joshua Menezes <112465506+Juiceyyyy@users.noreply.github.com>
2024-04-05 00:17:32 +0530

    Update connect.php

--- Eventopedia/config/connect.php
+++ Eventopedia/config/connect.php
@@ -1,29 +1,17 @@
 <?php
-// $con = mysqli_init();
-// mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
-// mysqli_real_connect($conn, "miniproj.mysql.database.azure.com", "miniproj1server", "Joshua*2024", "eventopedia", 3306, MYSQLI_CLIENT_SSL);
+// Azure MySQL database connection parameters
 $servername = 'miniproj.mysql.database.azure.com';
-$username='miniproj1server';
-$password='Joshua*2024';
-$dbname="eventopedia";
-function connect()
-{
+$username = 'miniproj1server';
+$password = 'Joshua*2024';
+$dbname = 'eventopedia';
 
-	global $servername, $username, $password, $dbname;
-	try{
-			$conn= new PDO("mysql:host=$servername; dbname=$dbname",$username,$password);
-			$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-			$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
-			return $conn;
-	}
-	catch(Exception $e){
-		echo "Connection Failed".$e->getmessage();
-	}
+// Attempt MySQL server connection
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
-function connect_close()
-{
-	$conn=null;
-}
-date_default_timezone_set("Asia/Calcutta");
-
 ?>
